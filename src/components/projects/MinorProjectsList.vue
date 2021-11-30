f01ec7f779e9bbd3838eb0bd2a4826fc86a202a5
Martin <33912290+machacekmartin@users.noreply.github.com>
2021-11-30 11:42:38 +0100

    Added minor projects' icons

--- src/components/projects/MinorProjectsList.vue
+++ src/components/projects/MinorProjectsList.vue
@@ -21,34 +21,34 @@ export default {
         return {
             minorProjects: [
                 {
-                    image: 'webp/pisecnice.webp',
+                    image: 'svg/icons/tictactoe.svg',
                     name: 'CLI Tic Tac Toe',
                     description: 'Resizable Tic Tac Toe in Node.js. Supports win-detection & coop multiplay for > 2 players',
                     link: 'https://github.com/machacekmartin',
                 },
                 {
-                    image: 'webp/pisecnice.webp',
+                    image: 'svg/icons/ascii.svg',
                     name: 'IMG & MP4 to ASCII convertor ',
                     description: 'Python CLI script for converting images and videos into ASCII format. Includes a simple bash script for playing output ASCII video',
-                    link: 'https://github.com/machacekmartin',
+                    link: 'https://github.com/machacekmartin/py-imgvideotoascii',
                 },
                 {
-                    image: 'webp/pisecnice.webp',
+                    image: 'svg/icons/resources.svg',
                     name: 'Resource Manager',
                     description: 'Web App for managing files, code snippets and links. Vue.js, SASS (BEM), Vuex, Axios, Laravel, MySQL',
-                    link: 'https://github.com/machacekmartin',
+                    link: 'https://github.com/machacekmartin/laravel-vue-resourcemngr',
                 },
                 {
-                    image: 'webp/pisecnice.webp',
+                    image: 'svg/icons/weather.svg',
                     name: 'Minimal Weather App',
                     description: 'Simple weather app with Nativescript, Typescript and Vue.js. Weather data is retrieved from Open Weather Map ',
-                    link: 'https://github.com/machacekmartin',
+                    link: 'https://github.com/machacekmartin/ns-minimalweather',
                 },
                 {
-                    image: 'webp/pisecnice.webp',
+                    image: 'svg/icons/slider.svg',
                     name: 'NPM Image Slider',
                     description: 'Simple and easy to use Image slider NPM package using Vue.js and SASS',
-                    link: 'https://github.com/machacekmartin',
+                    link: 'https://github.com/machacekmartin/vue-imageslider',
                 },
 
             ]
